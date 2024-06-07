d3ef71a73eb7c9e7aa2b77e35b6b9e7b4dd67ffb
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-07 16:44:13 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,14 @@
 <template>
-  <div>
-    <h2>Hello, {{ name }}</h2>
-    <div class="container">
-      <button v-on:click="flag = !flag">Click</button>
-      <h2 v-if="flag">Hello!</h2>
-      <h2 v-else>Bye Bye!</h2>
-    </div>
-    <div class="container-2">
-      <button @click="count = count + 1">Click + 1</button>
-      <p>{{ count }}</p>
-    </div>
-    <div class="container-3">
-      <input type="text" v-model="text" />
-      <p>{{ text }}</p>
-    </div>
-  </div>
+  <Cards />
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
+import Cards from "./components/Cards.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
-  setup() {
-    const name = "Roma RS";
-    const flag = ref(true);
-    const count = ref(0);
-    const text = ref("");
-    return { name, flag, count, text };
-  },
+  components: { Cards },
 });
 </script>
 
