04604c6d5370bbabefca894b4825285d5ede00a0
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-07 16:06:52 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Hello, {{ name }}</h2>
+    <div class="container">
+      <button v-on:click="flag = !flag">Click</button>
+      <h2 v-if="flag">Hello!</h2>
+      <h2 v-else>Bye Bye!</h2>
+    </div>
+    <div class="container-2">
+      <button @click="count = count + 1">Click + 1</button>
+      <p>{{ count }}</p>
+    </div>
+    <div class="container-3">
+      <input type="text" v-model="text" />
+      <p>{{ text }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, ref } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+    const name = "Roma RS";
+    const flag = ref(true);
+    const count = ref(0);
+    const text = ref("");
+    return { name, flag, count, text };
+  },
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h2 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
