1d31f4d1f3a104632fdd1672c0746787bfcbecc3
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-07 16:44:17 +0300

    Create Card.vue

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="card" v-for="card in arrayCard" :key="card.name">
+    <img :src="card.src" :alt="card.alt" style="width: 100px" />
+    <li>{{ card.name }} - {{ card.age }}</li>
+  </ul>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Card",
+  setup() {
+    return {};
+  },
+  props: {
+    arrayCard: Array,
+  },
+});
+</script>
+
+<style scoped></style>
