4e4b7f40385433a021b14d06708c3414c214f053
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-08 13:34:19 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,61 @@
 <template>
-  <div class="price-block">
-    <p>Original Price - {{ originalPrice }}</p>
-    <input type="number" name="" id="" v-model="discount" />
-    <p>Discount - {{ discount }}</p>
-    <p>Final Price - {{ finalPrice }}</p>
+  <div class="form-container">
+    <h2>{{ message }}</h2>
+    <form @submit.prevent="submitForm">
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="name" />
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email" />
+      <button type="submit">Submit</button>
+    </form>
+    <ul v-if="submissions.length > 0">
+      <li v-for="(submission, index) in submissions" :key="index">
+        <p>Name:{{ submission.name }}</p>
+        <p>Email:{{ submission.email }}</p>
+        <button @click="deleteSubmission(index)">Delete</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { defineComponent, computed, ref } from "vue";
-
-export default defineComponent({
+export default {
   name: "App",
-  components: {},
-  setup() {
-    const originalPrice = ref(100);
-    const discount = ref(0);
-    const finalPrice = computed(() => {
-      return originalPrice.value - discount.value;
-    });
-    return { originalPrice, discount, finalPrice };
+  data() {
+    return {
+      name: "",
+      email: "",
+      submissions: [],
+    };
   },
-});
+  methods: {
+    submitForm() {
+      const newSubmissions = {
+        name: this.name,
+        email: this.email,
+      };
+      this.submissions.push(newSubmissions);
+      this.name = "";
+      this.email = "";
+    },
+    deleteSubmission(index) {
+      this.submissions.splice(index, 1);
+    },
+    showMessage() {
+      alert("Hello World");
+    },
+  },
+  computed: {
+    message() {
+      const count = this.submissions.length;
+      if (count === 0) {
+        return "0 submissions";
+      } else {
+        return `${count} submissions`;
+      }
+    },
+  },
+};
 </script>
 
 <style>
