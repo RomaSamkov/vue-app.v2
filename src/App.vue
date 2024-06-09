8927882ab01e6a70d4001aad6ae75b18e3168d67
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-09 12:49:22 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,20 @@
 <template>
-  <div class="form-container">
-    <h2>{{ message }}</h2>
-    <form @submit.prevent="submitForm">
-      <label for="name">Name:</label>
-      <input type="text" id="name" v-model="name" />
-      <label for="email">Email:</label>
-      <input type="email" id="email" v-model="email" />
-      <button type="submit">Submit</button>
-    </form>
-    <ul v-if="submissions.length > 0">
-      <li v-for="(submission, index) in submissions" :key="index">
-        <p>Name:{{ submission.name }}</p>
-        <p>Email:{{ submission.email }}</p>
-        <button @click="deleteSubmission(index)">Delete</button>
-      </li>
-    </ul>
-  </div>
+  <input type="number" name="" id="" v-model="counter" />
+  <Counter :counter="counter" />
 </template>
 
 <script>
-export default {
+import { defineComponent, ref } from "vue";
+import Counter from "./components/Counter.vue";
+
+export default defineComponent({
+  components: { Counter },
   name: "App",
-  data() {
-    return {
-      name: "",
-      email: "",
-      submissions: [],
-    };
+  setup() {
+    const counter = ref(0);
+    return { counter };
   },
-};
+});
 </script>
 
 <style>
