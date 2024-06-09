76481360fe449e9414468061b1e66fafff8444b5
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-09 12:49:26 +0300

    Create Counter.vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>Counter (child component) {{ counter }}</h2>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  props: {
+    counter: Number,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style>
+.price-block {
+  text-align: center;
+}
+</style>
