17d43846fc2c395c887a3a81f5b143b74fcdabe7
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-10 20:45:28 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,49 @@
 <template>
-  <div class="main-block"><button @click="showModal">Show Modal</button></div>
+  <div class="main-block">
+    <button @click="showModal">Show Modal</button>
+    <br /><br /><br />
+    <label for="modalTitle">Title</label>
+    <input type="text" id="modalTitle" v-model="modalTitle" />
+    <label for="modalMessage">Message</label>
+    <input type="text" id="modalMessage" v-model="modalMessage" />
+    <Modal
+      v-if="isModealOpen"
+      :modal-title="modalTitle"
+      :modal-message="modalMessage"
+      @close="closeModal"
+    />
+  </div>
 </template>
 
 <script>
 import { defineComponent, ref } from "vue";
+import Modal from "./components/Modal.vue";
 
 export default defineComponent({
   name: "App",
+  components: { Modal },
   setup() {
     const isModealOpen = ref(false);
     const modalTitle = ref("");
     const modalMessage = ref("");
+
+    function showModal() {
+      isModealOpen.value = true;
+      modalTitle.value = "Default Title";
+      modalMessage.value = "Default Message";
+    }
+
+    function closeModal() {
+      isModealOpen.value = false;
+      modalTitle.value = "";
+      modalMessage.value = "";
+    }
     return {
       isModealOpen,
       modalTitle,
       modalMessage,
+      showModal,
+      closeModal,
     };
   },
 });
