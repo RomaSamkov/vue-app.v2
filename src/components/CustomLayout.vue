4ba98902dc11d1e9a840acf8a90844f1937cea30
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-11 14:34:47 +0300

    Create CustomLayout.vue

---
+++ src/components/CustomLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="custom-layout"><slot></slot></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CustomLayout",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.custom-layout {
+  padding: 20px;
+  margin: 0 auto;
+  max-width: 1200px;
+}
+</style>
