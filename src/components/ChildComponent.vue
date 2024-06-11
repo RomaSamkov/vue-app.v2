89edacd06d03e59058c81442a95971609ce8a3ec
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-11 12:06:15 +0300

    Create ChildComponent.vue

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="child-container">
+    <h2><slot name="title"></slot></h2>
+    <div><slot name="content"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style></style>
