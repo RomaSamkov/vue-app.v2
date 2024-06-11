61cc99372a19ab40e37c85e384cf1b8ce588ea2b
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-11 14:34:40 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <template>
-  <ParentComponent></ParentComponent>
+  <Header />
+  <CustomLayout>
+    <router-view></router-view>
+  </CustomLayout>
 </template>
 
 <script>
-import ParentComponent from "./components/ParentComponent.vue";
+import { defineComponent } from "vue";
+import Header from "./components/Header.vue";
+import CustomLayout from "./components/CustomLayout.vue";
 
-export default {
+export default defineComponent({
   name: "App",
-  components: { ParentComponent },
-};
+  components: { Header, CustomLayout },
+  setup() {
+    return {};
+  },
+});
 </script>
 
-<style></style>
+<style>
+ul {
+  list-style: none;
+  padding: 0;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
+a:hover {
+  text-decoration: underline;
+}
+</style>
