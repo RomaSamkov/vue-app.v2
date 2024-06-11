068aeb834f208049a05d05e20b110637ad84f15c
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-11 12:06:08 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,14 @@
 <template>
-  <div class="main-block">
-    <button @click="showModal">Show Modal</button>
-    <br /><br /><br />
-    <label for="modalTitle">Title</label>
-    <input type="text" id="modalTitle" v-model="modalTitle" />
-    <label for="modalMessage">Message</label>
-    <input type="text" id="modalMessage" v-model="modalMessage" />
-    <Modal
-      v-if="isModealOpen"
-      :modal-title="modalTitle"
-      :modal-message="modalMessage"
-      @close="closeModal"
-    />
-  </div>
+  <ParentComponent></ParentComponent>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
-import Modal from "./components/Modal.vue";
+import ParentComponent from "./components/ParentComponent.vue";
 
-export default defineComponent({
+export default {
   name: "App",
-  components: { Modal },
-  setup() {
-    const isModealOpen = ref(false);
-    const modalTitle = ref("");
-    const modalMessage = ref("");
-
-    function showModal() {
-      isModealOpen.value = true;
-      modalTitle.value = modalTitle.value || "Default Title";
-      modalMessage.value = modalMessage.value || "Default Message";
-    }
-
-    function closeModal() {
-      isModealOpen.value = false;
-      modalTitle.value = "";
-      modalMessage.value = "";
-    }
-    return {
-      isModealOpen,
-      modalTitle,
-      modalMessage,
-      showModal,
-      closeModal,
-    };
-  },
-});
+  components: { ParentComponent },
+};
 </script>
 
-<style>
-.price-block {
-  text-align: center;
-}
-</style>
+<style></style>
