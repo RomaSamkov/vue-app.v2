c4247c3cdd88084f858814b8b0c4f58dff068822
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-11 12:06:19 +0300

    Create ParentComponent.vue

---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="parent-container">
+    <h2>Parent Component</h2>
+    <ChildComponent>
+      <template v-slot:title>Child Component Title</template>
+      <template v-slot:content
+        >Child Component Content Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Nisi molestias voluptatum, reprehenderit minus,
+        corporis quae eaque, nemo porro rerum repellat sed minima totam itaque
+        dolore magni numquam laudantium soluta beatae.</template
+      >
+    </ChildComponent>
+  </div>
+</template>
+
+<script>
+import ChildComponent from "./ChildComponent.vue";
+
+export default {
+  name: "App",
+  components: { ChildComponent },
+};
+</script>
+
+<style></style>
