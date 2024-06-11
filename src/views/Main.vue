594348d12778cd8cdda64750a41da1ba9a6ce4a0
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-11 14:34:59 +0300

    Create Main.vue

---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div><h1>Welcome to shopping site!</h1></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Main",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style></style>
