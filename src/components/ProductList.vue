7286b60637394c6757a5c6ea3877f38fdd31b76f
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-12 12:48:30 +0300

    Create ProductList.vue

---
+++ src/components/ProductList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-if="!loading" class="content"></div>
+    <Loader v-else />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Loader from "./Loader";
+import { useProductStore } from "@/store";
+
+export default defineComponent({
+  name: "ProductList",
+  components: { Loader },
+  setup() {
+    const productStore = useProductStore();
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
