85d0df1112de0f8c0620ca447978010021184a9c
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-12 12:48:19 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <Header />
-  <CustomLayout>
-    <router-view></router-view>
-  </CustomLayout>
+  <div></div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import Header from "./components/Header.vue";
-import CustomLayout from "./components/CustomLayout.vue";
 
 export default defineComponent({
   name: "App",
-  components: { Header, CustomLayout },
+  components: {},
   setup() {
     return {};
   },
@@ -20,15 +15,25 @@ export default defineComponent({
 </script>
 
 <style>
+* {
+  font-family: Lexend, sans-serif;
+}
+
 ul {
   list-style: none;
   padding: 0;
 }
-a {
-  text-decoration: none;
-  color: black;
-}
-a:hover {
-  text-decoration: underline;
+
+button {
+  cursor: pointer;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  padding: 5px 10px;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 19px;
+  text-align: center;
+  color: #fff;
+  background: #79c56e;
+  border-radius: 6px;
 }
 </style>
