9c534121367141b1ddf12090721fa33af6e738b0
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-15 20:04:15 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Main from "../views/Main.vue";
-import Contact from "../views/Contact.vue";
-import ProductList from "../views/ProductList.vue";
-import Product from "../views/Product.vue";
+import Main from '@/components/Main'
+import About from '@/components/About'
 
 const routes = [
-  {
-    path: "/",
-    name: Main,
-    component: Main,
-  },
-  {
-    path: "/products",
-    name: ProductList,
-    component: ProductList,
-  },
-  {
-    path: "/products/:id",
-    name: Product,
-    component: Product,
-    props: true,
-  },
-  {
-    path: "/contact",
-    name: Contact,
-    component: Contact,
-  },
-];
+    {
+        path: '/',
+        name: 'Main',
+        component: Main
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: routes,
-});
+    history: createWebHistory(),
+    routes
+})
 
-export default router;
+export default router
